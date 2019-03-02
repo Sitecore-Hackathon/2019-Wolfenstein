a13415b4feb967d8667bb252533ade8e7169b60d
Jorge Cardenas <jorge.cardenas@verndale.com>
2019-03-02 09:17:00 -0500

    Component working

--- src/Feature/Media/HackathonWeb.Feature.Media/Models/ImageFilterComponentModel.cs
+++ src/Feature/Media/HackathonWeb.Feature.Media/Models/ImageFilterComponentModel.cs
@@ -1,22 +1,19 @@
-﻿using System.Drawing;
-using System.Web.Mvc;
-using Sitecore.XA.Feature.Media.Models;
+﻿using Sitecore.XA.Feature.Media.Models;
 
 namespace HackathonWeb.Feature.Media.Models
 {
     public class ImageFilterComponentModel : ImageRenderingModel
     {
-        public Sitecore.Data.ID Id{get;set;}
+        public Sitecore.Data.ID Id { get; set; }
+
         public bool HasImageCaption { get; set; }
 
         public bool HasImageDescription { get; set; }
 
-        
-
         public string Filter { get; set; }
 
-        public Bitmap OriginalImage { get; set; }
+        public string OriginalImage { get; set; }
 
-        public Bitmap FilterImage { get; set; }
+        public string FilterImage { get; set; }
     }
 }
